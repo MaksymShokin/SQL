e886386c84a7c8ffdd56391d2bfe7ca97b72d92a
Maksym Shokin <maksym.sh@upplabs.com>
2020-06-02 23:27:56 +0300

    deleting-item-from-cart

--- controllers/shop.js
+++ controllers/shop.js
@@ -102,13 +102,20 @@ exports.getOrders = (req, res, next) => {
 };
 
 exports.postCartDeleteItem = (req, res, next) => {
-  const productId = req.body.productId;
-
-  Product.getById(productId, (product) => {
-    Cart.deleteProduct(productId, product.price);
-  })
-  
-  res.redirect('/cart')
+  const prodId = req.body.productId;
+  req.user
+    .getCart()
+    .then(cart => {
+      return cart.getProducts({ where: { id: prodId } });
+    })
+    .then(products => {
+      const product = products[0];
+      return product.cartItem.destroy();
+    })
+    .then(result => {
+      res.redirect('/cart');
+    })
+    .catch(err => console.log(err));
 }
 
 exports.getCheckout = (req, res, next) => {
